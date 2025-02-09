220e5704846e5547ebfe5ff6a5eb673492f7f2d7
MajoRC221051 <110446328+MajoRC221051@users.noreply.github.com>
2025-02-08 20:09:58 -0600

    Update DataGenerator.java

--- src/DataGenerator.java
+++ src/DataGenerator.java
@@ -1,13 +1,22 @@
-// DataGenerator.java
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
 
 public class DataGenerator {
+    
+    /** 
+     * @param filename
+     */
     public void generateData(String filename) {
         Random rand = new Random();
-        
-        try (FileWriter writer = new FileWriter(filename)) {
+        File file = new File(filename);
+
+        if (file.exists()) {
+            System.out.println("El archivo ya existe y será sobrescrito: " + filename);
+        }
+
+        try (FileWriter writer = new FileWriter(file, false)) { 
             for (int i = 0; i <= 3000; i++) {
                 writer.write(rand.nextInt(1000) + "\n");
             }
